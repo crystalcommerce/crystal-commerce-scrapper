52ec121082b087992e78ddfc8956084a743ffeb4
Omid <omid@ubuntu.ubuntu-domain>
2020-02-23 09:33:58 +0300

    fix things

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const http = require('http');
+const exphbs = require('express-handlebars');
 const app = express()
 const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug');
+app.engine('hbs', exphbs({ extname: '.hbs'}));
+app.set('view engine', 'hbs');
 app.set('views', './views');
+app.use(express.static('public'))
 app.get('/', (req, res) => res.render('index'));
+app.get('/dashboard', (req, res) => res.render('admin_home'));
+app.get('/download', (req, res) => res.render('admin_home'));
+app.get('/add-new', (req, res) => res.render('admin_home'));
+app.get('/status', (req, res) => res.render('admin_home'));
+app.get('/logout', (req, res) => res.render('admin_home'));
+
 http.createServer(app).listen(port);
