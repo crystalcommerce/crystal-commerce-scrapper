27cfbb5e709aba6d49510d83f47f01996fd50c37
Omid <omid@ubuntu.ubuntu-domain>
2020-02-27 09:06:33 +0300

    add login and dashboard to project

--- index.js
+++ index.js
@@ -3,19 +3,80 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const http = require('http');
 const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const session = require('express-session');
+const mongoose=require('mongoose')
+const flash = require('connect-flash');
+
 const app = express()
 const port = 3000;
-app.use(bodyParser.json());
+const { ensureAuthenticated, forwardAuthenticated } = require('./config/auth');
+
+ require('./config/passport')(passport);
+
+
+const db = require('./config/keys').mongoURI;
+
+
+mongoose
+  .connect('mongodb://localhost:27017/exampleDb',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(flash());
+
+
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+
 app.engine('hbs', exphbs({ extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', './views');
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+
+app.use('/users', require('./routes/users.js'));
+
 app.get('/', (req, res) => res.render('index'));
-app.get('/dashboard', (req, res) => res.render('admin_home'));
+app.get('/dashboard',ensureAuthenticated, (req, res) =>
+res.render('admin_home', {
+  user: req.user
+}));
 app.get('/download', (req, res) => res.render('admin_home'));
 app.get('/add-new', (req, res) => res.render('admin_home'));
 app.get('/status', (req, res) => res.render('admin_home'));
 app.get('/logout', (req, res) => res.render('admin_home'));
+app.get('/register',(req,res)=>res.render('register'));
+app.get('/users',(req,res)=>res.render('users'))
+///
 
 http.createServer(app).listen(port);
---
+++ routes/users.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+var bodyParser = require('body-parser')
+
+const passport = require('passport');
+// Load User model
+const User = require('../models/User');
+const { forwardAuthenticated } = require('../config/auth');
+
+
+var app = express()
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+router.get('/login', forwardAuthenticated, (req, res) => res.render('index'));
+
+
+router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
+
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+console.log(req.body)
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email already exists' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  'success_msg',
+                  'You are now registered and can log in'
+                );
+                res.redirect('/users/login');
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  }
+});
+// Login
+
+router.post('/login',urlencodedParser, (req, res, next) => {
+  console.log('login starting ...')
+  console.log(req.body)
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+
+
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
