57668997311791a18b13672a95340cfb63957b65
sms7182 <sms7182@gmail.com>
2020-03-26 13:27:39 +0430

    fix bug

--- helpers/scrapper-scheduler.js
+++ helpers/scrapper-scheduler.js
@@ -3,7 +3,14 @@ const Scrap= require('../models/Scrap');
 
 let runningScrapper = [];
 
+function diff_minutes(dt2, dt1) 
+ {
 
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60;
+  return Math.abs(Math.round(diff));
+  
+ }
 var CronJob = require('cron').CronJob;
 var job = new CronJob('* * * * *', async function () {
 
@@ -19,19 +26,33 @@ var job = new CronJob('* * * * *', async function () {
 
         if (rss.length != 0) continue;
         var d=new Date();
-        var comp= s.lastDoneDate-d;
+    
+       
+       var comp= diff_minutes(d,s.lastDoneDate);
+        console.log('date time is ',d)
+        console.log('last done ',s.lastDoneDate)
+        console.log('compare is ',comp)
         if(comp===0){
          console.log('same time and should do',comp)
          s.lastDoneDate.setTime(s.lastDoneDate.getTime()+s.everyMinute*60000);
-         s.save();
+         Scrap.findOne({ _id: s.id }, function (err, doc){
+            doc.lastDoneDate =s.lastDoneDate.getTime()+(s.everyMinute*60000);
+            
+            doc.save();
+          });
+         
         }
-        else if(comp<0){
+        else if(comp>0){
             console.log('last done date is passed and should do',comp)
             s.lastDoneDate.setTime(d.getTime()+s.everyMinute*60000);
-            s.save()
+            Scrap.findOne({ _id: s.id }, function (err, doc){
+                doc.lastDoneDate =d.getTime()+(s.everyMinute*60000);
+                
+                doc.save();
+              });
         }
         else{
-            
+
            console.log('date earlier',comp)
            continue;
         }
